f65aa98e23921143b5ac32ccb527390dffb96981
newton <newtonoyagha@gmail.com>
2025-10-28 00:45:37 +0100

    set up tools and memory for agent

--- agent.js
+++ agent.js
@@ -1,17 +1,50 @@
 import { createAgent } from "langchain";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {tool} from "@langchain/core/tools"
+import {tavily} from "@tavily/core"
+import { MemorySaver } from "@langchain/langgraph";
+import {z} from "zod"
 import dotenv from "dotenv"
 dotenv.config()
 
+
+
+//models
+
+const tavilyClient = tavily({
+      apiKey:process.env.TAVILY_API
+})
 
 const model = new ChatGoogleGenerativeAI({
       model: 'gemini-2.5-flash',
       apiKey:process.env.GOOGLE_GEMINI_API
 })
 
+//tool
+const searchTool = tool(async ({query})=> {
+      
+      return await tavilyClient.search(query)
+}, {
+      name:"websearch",
+      description:`Search the web in real-time for scholarships and funding opportunities worldwide. "
+    "Use this tool to find current, reliable, and relevant scholarship information.
+`,
+        schema: z.object({
+            query:z.string()
+        })
+}
+
+)
+
+//memory
+const checkpointSaver = new MemorySaver()
+
+//agent
+
 const agent = createAgent({
       model,
-      tools:[]
+      tools:[searchTool],
+      checkpointer:checkpointSaver
  
 });
 
@@ -19,8 +52,24 @@ const res = await agent.invoke({
       messages: [
             {
                   role:"user",
-                  content:"hiii"
+                  content:"scholarship in us"
             }
       ]
+}, {
+      configurable:{thread_id:41}
 })
-console.log(res)
+
+
+const res2 = await agent.invoke({
+      messages: [
+            {
+                  role:"user",
+                  content:"what coutry are we talking about?"
+            }
+      ]
+}, {
+      configurable:{thread_id:41}
+})
+
+console.log(res.messages.at(-1)?.content)
+console.log(res2.messages.at(-1)?.content)
